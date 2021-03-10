afb88e12d0a077257292d818b71313615e781cc3
Anastasya Lukovskaya <fafnirra@gmail.com>
2021-03-10 23:01:44 +0300

    Lesson 1 Task 2 v2

---
+++ Lesson1Base/Lesson1Base_Task2_Lukovskaya/Lesson1Base_Task2_Lukovskaya/Lesson1Base_Task2_Lukovskaya.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson1Base_Task2_Lukovskaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите рост в метрах.\nИспользуйте запятую для разделения метров и сантиметров, например '1,70': ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите вес: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            double BMI = m / (h + h);
+
+            Console.WriteLine($"Индекс массы тела равен: {BMI.ToString("###. ##")}\nНормой считаются показатели от 18,50 до 24,99 килограмм на квадратный метр.\nЕсли результат находится вне этого диапазона, необходима консультация специалиста.");
+        }
+    }
+}
