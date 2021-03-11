7f60f443ddcfd7e1e259f1520223fa75ddbd1ecc
Anastasya Lukovskaya <fafnirra@gmail.com>
2021-03-11 11:26:59 +0300

    5

---
+++ Lesson1Base/Lesson1Base_Task5_Lukovskaya.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson1Base_Task5_Lukovskaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine("Анастасия Луковская, Москва");
+        }
+    }
+}
